ccaeb385f1dc82815d6a95bad5a6e741522edf3a
Densingh <ddensingh19@gmail.com>
2025-02-04 23:44:24 +0530

    button removed

--- src/users/Users.jsx
+++ src/users/Users.jsx
@@ -38,9 +38,7 @@ const Users = () => {
 
   return (
     <div className="users-page" onClick={handleCloseSidebar}>
-      {/* <button className="hamburger" onClick={(e) => {e.stopPropagation(); handleToggleSidebar();}}>
-        <FaBars />
-      </button> */}
+     
       <SlideBar isOpen={sidebarOpen} onClose={handleCloseSidebar} />
       <div className="users-container">
         <h2>
